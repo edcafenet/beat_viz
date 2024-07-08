f6049c522b161b36078f6d7f227ae4f4c449bab8
Eduardo Castello <edcafenet@gmail.com>
2024-07-08 15:28:41 +0200

    corrected coordinates with optritrack

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import struct
 import numpy as np
 import random
 
-UDP_IP = "10.205.10.13"
+UDP_IP = "10.205.3.229"
 UDP_PORT = 9876
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -14,16 +14,16 @@ while True:
     try:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         data_string = list(data.decode()[1:-1].split(','))
-        data_int = [int(round(float(i)*10)) for i in data_string]
-        data_int[1] += 1500
-        data_int[0] += 600
+        data_int = [int(round(float(i),1)*10) for i in data_string]
+        corrected_x = data_int[0] + 338
+        corrected_y = -data_int[1] + 124
 
-        # pyautogui.drag(random.randint(0, 10), random.randint(0, 10), 1, button='left') 
-
-        pyautogui.moveTo(data_int[1], data_int[0])
+        pyautogui.moveTo(corrected_x, corrected_y)
         #pyautogui.click()
         #pyautogui.mouseDown()
-        #print(data_int[1], data_int[0])
 
+
+        # pyautogui.drag(random.randint(0, 10), random.randint(0, 10), 1, button='left') 
+        
     except KeyboardInterrupt:
         pass
