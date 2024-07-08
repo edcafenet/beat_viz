b38aecd0eb10603ec8791d2a41086e9450c8cb44
Eduardo Castello <edcafenet@gmail.com>
2024-07-08 14:19:40 +0200

    new version with FFT analyzer

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ import socket
 import pyautogui
 import struct
 import numpy as np
-import keyboard
 import random
-from audiohandler import audiohandler
 
-UDP_IP = "10.205.3.229"
+UDP_IP = "10.205.10.13"
 UDP_PORT = 9876
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
-audio = audiohandler()
-audio.start()     # open the the stream
 
 while True:
     try:
@@ -24,16 +17,13 @@ while True:
         data_int = [int(round(float(i)*10)) for i in data_string]
         data_int[1] += 1500
         data_int[0] += 600
-  
-        # if audio.beat_present():
-        #     pyautogui.drag(random.randint(0, 10), random.randint(0, 10), 1, button='left') 
-        if audio.beat_present():
-            pyautogui.moveTo(data_int[1], data_int[0])
-            pyautogui.click()
-            pyautogui.mouseDown()
 
-        print(data_int[1], data_int[0])
+        # pyautogui.drag(random.randint(0, 10), random.randint(0, 10), 1, button='left') 
+
+        pyautogui.moveTo(data_int[1], data_int[0])
+        #pyautogui.click()
+        #pyautogui.mouseDown()
+        #print(data_int[1], data_int[0])
 
     except KeyboardInterrupt:
-        audio.stop()
         pass
