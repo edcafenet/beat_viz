748882c67b19ac7596eda31c8fe8dcf6fd19a775
Eduardo Castello <edcafenet@gmail.com>
2024-07-08 16:12:58 +0200

    scaled x,y

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import struct
 import numpy as np
 import random
 
-UDP_IP = "10.205.3.229"
+UDP_IP = "10.205.3.4"
 UDP_PORT = 9876
+
+scaling_x = 1.40
+scaling_y = 1.56
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
@@ -18,12 +21,15 @@ while True:
         corrected_x = data_int[0] + 338
         corrected_y = -data_int[1] + 124
 
-        pyautogui.moveTo(corrected_x, corrected_y)
+        scaled_x = corrected_x * scaling_x
+        scaled_y = corrected_y * scaling_y
+
+        pyautogui.moveTo(scaled_x, scaled_y)
         #pyautogui.click()
         #pyautogui.mouseDown()
 
 
         # pyautogui.drag(random.randint(0, 10), random.randint(0, 10), 1, button='left') 
-        
+
     except KeyboardInterrupt:
         pass
