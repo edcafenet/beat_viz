8dde35b4f9ba751c776c2cd423843cc064c13f6a
Eduardo Castello <edcafenet@gmail.com>
2024-08-03 22:40:46 +0200

    first version with astar algorithm

--- fft_analyzer.py
+++ fft_analyzer.py
@@ -11,18 +11,26 @@ class fft_analyzer:
     sound_device_index = None
     visualizer = None
     
-    random_increment_x = 0
-    random_increment_y = 0
-    current_mouse_position = [0,0]
-
-    go = None
-    previous_beat = 0
-    current_beat = 0
-    test_update = time.time()
-    
-    def __init__(self, sound_device_index, visualizer):
+    def __init__(self, sound_device_index):
         self.sound_device_index = sound_device_index
-        self.visualizer = visualizer
+        self.visualizer = False
+
+        self.ear = Stream_Analyzer(
+                        device = self.sound_device_index, # Pyaudio (portaudio) device index, defaults to first mic input
+                        rate   = None,                    # Audio samplerate, None uses the default source settings
+                        FFT_window_size_ms  = 60,         # Window size used for the FFT transform
+                        updates_per_second  = 500,        # How often to read the audio stream for new data
+                        smoothing_length_ms = 50,         # Apply some temporal smoothing to reduce noisy features
+                        n_frequency_bins = 8,             # The FFT features are grouped in bins
+                        visualize = self.visualizer,      # Visualize the FFT features with PyGame
+                        verbose   = 0,                    # Print running statistics (latency, fps, ...)
+                        height    = 450,                  # Height, in pixels, of the visualizer window,
+                        window_ratio = 24/9               # Float ratio of the visualizer window. e.g. 24/9
+                        )
+
+        self.fps = 60  #How often to update the FFT features + display
+        self.last_update = time.time()
+        print("All ready, starting audio measurements now...")
 
     def beat_present(self, bin_id, threshold):
         if self.binned_fft[bin_id] > threshold:
@@ -30,68 +38,7 @@ class fft_analyzer:
         else:
             return False
 
-    def autonomous_mode(self):
-        self.current_beat = self.binned_fft[0]
-
-        if self.beat_present(0, 20):
-            if self.current_mouse_position[0] <= 25:
-                self.go = True
-            elif self.current_mouse_position[0] > (pyautogui.size()[0] - 25):
-                self.go = False
-
-            jump = round(self.binned_fft[0]/2)
-
-            if self.go:
-                self.current_mouse_position[0] += jump
-            if not self.go:
-                self.current_mouse_position[0] -= jump 
-
-            self.current_mouse_position[1] = 650
-
-            self.current_mouse_position[0] = self.current_mouse_position[0] - self.random_increment_x
-            self.current_mouse_position[1] = self.current_mouse_position[1] - self.random_increment_y
-            
-            self.random_increment_x = random.randrange(-jump,jump)
-            self.random_increment_y = random.randrange(-jump,jump)
-
-            pyautogui.mouseDown()
-            pyautogui.moveTo(self.current_mouse_position[0] + self.random_increment_x, self.current_mouse_position[1] + self.random_increment_y)
-            pyautogui.mouseUp()
-
-        # print(self.previous_beat)
-        # print(self.current_beat)
-        # print("--------")
-
-        if round(self.current_beat - self.previous_beat) > 16:
-            pyautogui.press('space') 
-        
-        if (time.time() - self.test_update) > (1./5):
-            self.test_update = time.time()
-            self.previous_beat = self.current_beat
-
-
     def run(self):
-        ear = Stream_Analyzer(
-                        device = self.sound_device_index, # Pyaudio (portaudio) device index, defaults to first mic input
-                        rate   = None,               # Audio samplerate, None uses the default source settings
-                        FFT_window_size_ms  = 60,    # Window size used for the FFT transform
-                        updates_per_second  = 500,   # How often to read the audio stream for new data
-                        smoothing_length_ms = 50,    # Apply some temporal smoothing to reduce noisy features
-                        n_frequency_bins = 8,        # The FFT features are grouped in bins
-                        visualize = self.visualizer, # Visualize the FFT features with PyGame
-                        verbose   = 0,    # Print running statistics (latency, fps, ...)
-                        height    = 450,     # Height, in pixels, of the visualizer window,
-                        window_ratio = 24/9  # Float ratio of the visualizer window. e.g. 24/9
-                        )
-
-        fps = 60  #How often to update the FFT features + display
-        last_update = time.time()
-        print("All ready, starting audio measurements now...")
-        
-        while True:
-            if (time.time() - last_update) > (1./fps):
-                last_update = time.time()
-                self.raw_fftx, self.raw_fft, self.binned_fftx, self.binned_fft = ear.get_audio_features()
-                self.autonomous_mode()
-
-                
+        if (time.time() - self.last_update) > (1./self.fps):
+            self.last_update = time.time()
+            self.raw_fftx, self.raw_fft, self.binned_fftx, self.binned_fft = self.ear.get_audio_features()                
--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from threading import Thread
 from fft_analyzer import fft_analyzer
 import random
 import argparse
+from astar import astar
+import numpy as np
+
+x = 0
+y = 0
+z = 0
 
 class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
-
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
         current_thread = threading.current_thread()
-        print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
+        #print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
 
         data_string = list(data.decode()[1:-1].split(','))
         data_int = [int(round(float(i),1)*10) for i in data_string]
@@ -19,20 +24,145 @@ class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
         corrected_x = data_int[0] + 225 
         corrected_y = -data_int[1] + 79
 
-        scaling_x = 0.521
-        scaling_y = 0.521
+        scaling_factor = 0.521
+        
+        scaled_y = (corrected_y * scaling_factor)          
+        scaled_x = (corrected_x * scaling_factor)
 
-        scaled_y = (corrected_y * scaling_y)          
-        scaled_x = (corrected_x * scaling_x)
-
-        scaled_y = scaled_y + 120
-        scaled_x = scaled_x 
-
-        pyautogui.moveTo(scaled_x, scaled_y)
+        global x, y, z
+        y = scaled_y + 120
+        x = scaled_x 
+        z = data_int[2]
+ 
         socket.sendto(data.upper(), self.client_address)
 
-class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):    
     pass
+
+class beat_viz():
+    def __init__(self, device_id):
+        self.fft = fft_analyzer(device_id)
+        
+        for i in range(10):
+            self.fft.run()
+            time.sleep(0.1)
+        
+        self.time_start = time.time()
+        self.current_mouse_position = [0,650]
+        self.new_mouse_position = [0,0]
+        self.random_increment = [0,0]
+    
+        self.current_beat = 0
+        self.previous_beat = 0
+
+        self.threshold = 20
+
+        self.path_counter = 0
+
+        # Direction of the movement
+        self.direction_forward = None
+
+    def autonomous_line(self):        
+        # Run the FFT and retrieve a value for the first bin
+        self.fft.run()
+        self.current_beat = self.fft.binned_fft[0]
+
+        if self.fft.beat_present(0, self.threshold):
+            if self.current_mouse_position[0] <= 25:
+                self.direction_forward = True
+            elif self.current_mouse_position[0] > (pyautogui.size()[0] - 25):
+                self.direction_forward = False
+
+            jump = round(self.current_beat/2)
+
+            if self.direction_forward:
+                self.current_mouse_position[0] += jump
+            if not self.direction_forward:
+                self.current_mouse_position[0] -= jump 
+
+            # Remove the previous random movement
+            self.current_mouse_position[0] = self.current_mouse_position[0] - self.random_increment[0]
+            self.current_mouse_position[1] = self.current_mouse_position[1] - self.random_increment[1]
+            
+            # Calculate the next random movement
+            self.random_increment = [random.randrange(-jump,jump), random.randrange(-jump,jump)]
+
+            # Caculate new position
+            self.new_mouse_position = [self.current_mouse_position[0] + self.random_increment[0], self.current_mouse_position[1] + self.random_increment[1]]
+
+            # Move the mouse
+            pyautogui.mouseDown()
+            pyautogui.moveTo(self.new_mouse_position[0], self.new_mouse_position[1])
+            pyautogui.mouseUp()
+
+            # Update position
+            self.current_mouse_position = self.new_mouse_position
+
+        # If there is a drop press 'space'
+        if round(self.current_beat - self.previous_beat) > self.threshold:
+            pyautogui.press('space') 
+        
+        # Calculate the time window for calculate the drop above
+        if  (time.time() - self.time_start) > (1./5):
+            self.time_start = time.time()
+            self.previous_beat = self.current_beat
+
+    def autonomous_path(self, path):        
+        self.fft.run()
+        self.current_beat = self.fft.binned_fft[0]
+
+        if self.fft.beat_present(0, self.threshold):
+            jump = round(self.current_beat/2)
+          
+            if self.path_counter < len(path):
+                self.current_mouse_position[0] = path[self.path_counter][0]
+                self.current_mouse_position[1] = path[self.path_counter][1]
+            else:
+                self.path_counter = 0
+
+            # Remove the previous random movement
+            self.current_mouse_position[0] = self.current_mouse_position[0] - self.random_increment[0]
+            self.current_mouse_position[1] = self.current_mouse_position[1] - self.random_increment[1]
+            
+            # Calculate the next random movement
+            self.random_increment = [random.randrange(-jump,jump), random.randrange(-jump,jump)]
+
+            # Caculate new position
+            self.new_mouse_position = [self.current_mouse_position[0] + self.random_increment[0], self.current_mouse_position[1] + self.random_increment[1]]
+
+            # Drag the mouse
+            pyautogui.dragTo(self.new_mouse_position[0], self.new_mouse_position[1], button='left')
+
+            # Update position
+            self.current_mouse_position = self.new_mouse_position
+
+            # Update counter
+            self.path_counter += 1
+
+        # If there is a drop press 'space'
+        if round(self.current_beat - self.previous_beat) > self.threshold:
+            pyautogui.press('space') 
+        
+        # Calculate the time window for calculate the drop above
+        if  (time.time() - self.time_start) > (1./10):
+            self.time_start = time.time()
+            self.previous_beat = self.current_beat
+        
+    def run(self):
+        grid = np.ones((pyautogui.size()[0], pyautogui.size()[1]))
+
+        for row in range(0, 500):
+            for column in range(500, 1000):
+                grid[column][row] = 0
+
+        a = astar(grid)
+        path = a.search([75,200], [75, 650])
+        path = path + a.search([75, 650], [1400, 650])
+        path = path + a.search([1400, 650], [1400, 200])
+        path = path + a.search([1400, 200], [75, 200])
+
+        while True:
+            self.autonomous_path(path)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -47,18 +177,17 @@ def parse_args():
     return parser.parse_args()
 
 if __name__ == "__main__":
-
     args = parse_args()
     server = ThreadedUDPServer((args.ip, args.port), ThreadedUDPRequestHandler)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
 
-    fft = fft_analyzer(args.device, args.visualizer)
+    viz = beat_viz(args.device)
 
     try:
         server_thread.start()
         print("Server started at {} port {}".format(args.ip, args.port))
-        fft.run()
+        viz.run()
 
     except (KeyboardInterrupt, SystemExit):
         server.shutdown()
